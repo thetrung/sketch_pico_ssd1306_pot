71efea971860a85e27c6d4af26223e328b089f2e
Trung Nguyen <thetrung@Mac-mini.local>
2025-06-14 23:31:52 +0700

    Last working version.

--- sketch_pico_oled_pot.ino
+++ sketch_pico_oled_pot.ino
@@ -79,6 +79,7 @@ void setup() {
   pinMode(EC11_B, INPUT);
   pinMode(EC11_D, INPUT_PULLUP);
 
+  delay(1000);
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
